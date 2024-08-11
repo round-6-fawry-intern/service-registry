79b388193510a5d2e5d4962aff4c8a0045427f68
Ahmed Ezz <ahmed.ben.ezz@gmail.com>
2024-08-11 13:52:00 -0700

    first commit

---
+++ src/test/java/com/example/serviceregister/ServiceregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
